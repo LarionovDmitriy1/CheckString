04b8a9bb3b04cb7a5cf8ecf58cb8a7f6e95d5407
Студент1 <1@DESKTOP-7RO4MU5>
2022-12-20 11:38:14 +0300

    Добавьте файлы проекта.

---
+++ CheckString/Program.cs
@@ -0,0 +1,4 @@
+﻿using CheckString;
+Console.WriteLine("Введите строку");
+var result = Validator.WorkVolidator(Console.ReadLine());
+Console.WriteLine(result);
---
+++ CheckString/Validator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckString;
+
+public class Validator
+{
+    public static bool WorkVolidator(string str)
+    {
+        bool result = false;
+        if (str.Length == 0 || string.IsNullOrEmpty(str)) { return false; }
+        int count = 0;
+        char[] chars = new char[] { '_', ' ', '-', ';', ':', ',', '/', '*', '+' };
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (chars.Contains(str[i]))
+            {
+                return false;
+            }
+            for (int j = 0; j < str.Length - 1; j++)
+            {
+                if (char.IsDigit(str[j]) && char.IsDigit(str[j + 1]))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
